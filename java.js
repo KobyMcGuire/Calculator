8d7d1ff78e4cbbcde378e4dd3a172bbf4698c39b
Koby <mcguirej2019@gmail.com>
2022-11-09 16:52:23 -0500

    Add the math functions in js
    The basic math functions were added along with an operate function that
    decides which function to call based on the operator that is passed to
    it.

--- java.js
+++ java.js
@@ -1,1 +1,41 @@
-console.log("hi");
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, a, b) {
+    if (operator === "+") {
+        evaluation = add(a,b);
+    }
+
+    else if (operator === "-") {
+        evaluation = subtract(a, b);
+    }
+
+    else if (operator === "*") {
+        evaluation = multiply(a, b);
+    }
+
+    else if (operator === "/") {
+        evaluation = divide(a, b);
+    }
+
+    return evaluation;
+
+}
+
+console.log(operate("+", 2, 3));
+console.log(operate("-", 2, 3));
+console.log(operate("/", 2, 3));
+console.log(operate("*", 2, 3));
