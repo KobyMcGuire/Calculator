7b837d2afab2213d5d69df9cd7561a2fed5e419a
Koby <mcguirej2019@gmail.com>
2022-11-10 17:28:44 -0500

    Bring calculator up to basic functioning
    The calculator is now functional and can accept one number, an operator,
    and one other number. More work needs to be done to insure other cases
    are fulfilled.

--- java.js
+++ java.js
@@ -15,6 +15,7 @@ function divide(a, b) {
 }
 
 function operate(operator, a, b) {
+
     if (operator === "+") {
         evaluation = add(a,b);
     }
@@ -35,15 +36,63 @@ function operate(operator, a, b) {
 
 }
 
-let viewer = document.querySelector(".viewer");
-function displayNumbers(button) {
-    viewer.innerHTML = `${button.textContent}`;
+function clearViewer() {
+    viewer.innerHTML = '';
+    return;
 }
 
+function displayNumbers(button) {
+    if (button.id === "operator") {
+        clearViewer();
+        viewer.innerHTML = button.textContent;
+        return;
+    }
+    // if for clear
+    // if for enter
+    viewer.innerHTML += `${button.textContent}`;
+}
+
+
 let buttons = document.querySelectorAll(".button");
+let viewer = document.querySelector(".viewer");
+let arr = [];
+
 for (const button of buttons) {
     button.addEventListener("click", function() {
-        displayNumbers(button);
+
+        //Will clear the viewer after an operator is chosen
+        if (viewer.innerHTML === "+" || 
+            viewer.innerHTML === "-" ||
+            viewer.innerHTML === "*" ||
+            viewer.innerHTML === "/") {
+                
+                clearViewer();
+        }
+
+        //Seperate from above
+        if (button.id === "number") {
+            displayNumbers(button);
+        }
+
+        if (button.id === "operator") {
+            arr.push(parseInt(viewer.innerHTML));
+            arr.push(button.textContent);
+            displayNumbers(button);  
+        }
+
+        if (button.id === "equals") {
+            arr.push(parseInt(viewer.innerHTML));
+            clearViewer();
+            let evaluation = operate(arr[1], arr[0], arr[2]);
+            viewer.innerHTML = `${evaluation}`;
+        }
+
+        if (button.id === "clear") {
+            clearViewer();
+            arr = [];
+        }
+
+        console.log(arr);
     })
 }
 
